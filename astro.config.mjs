77ddeec5177073b90c1edb61485f6dff1c7123bc
ScotiaNova <songjienova@gmail.com>
2025-10-22 01:24:50 +0800

    fix: 添加仓库名

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ScotiaNova.github.io",
-	base: "/",
+	base: "/SjBlog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
