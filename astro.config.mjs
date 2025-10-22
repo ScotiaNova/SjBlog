26eff20d1a051044bad1486b754d1eca84c0c735
ScotiaNova <songjienova@gmail.com>
2025-10-22 12:38:47 +0800

    redo

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://ScotiaNova.github.io",
-	base: "/SjBlog",
+	site: "https://www.scotianova.top",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
